2bebe2746821479ebca2118889f8cfef36a7b667
syedtalhahamid <syedtalhacodenera@gmail.com>
2025-09-16 13:37:50 +0530

    initial

---
+++ app.py
@@ -0,0 +1,12 @@
+# pip install flask
+
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def demo():
+    return render_template("demo.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
